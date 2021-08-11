fd323d0861bddeab9ee2c33bfe73532396301585
ww020823 <deityxsky@foxmail.com>
2021-08-11 15:03:24 +0800

    task.sh

---
+++ task.sh
@@ -0,0 +1,6 @@
+python train_x.py --config configs/train-rs/train_ITCVD_drsensx2.yaml --name ITCVD_drsensx2
+python train_x.py --config configs/train-rs/train_ITCVD_drsenmx2.yaml --name ITCVD_drsenmx2
+python train_x.py --config configs/train-rs/train_ITCVD_drsenCAx2.yaml --name ITCVD_drsencax2
+#python train_x.py --config configs/train-rs/train_ITCVD_edmkbx2.yaml --name ITCVD_edmkbx2
+
+
