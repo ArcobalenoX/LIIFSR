b3e4d32b8f0aee9f91aa394de571e76c0c48f519
arcobaleno <deityxsky@foxmail.com>
2021-11-03 08:39:10 +0800

    test l0

--- test_L0.py
+++ test_L0.py
@@ -12,64 +12,7 @@ sys.path.append("models")
 import datasets
 import models
 import utils
-
-
-def batched_predict(model, inp):
-    model.eval()
-    with torch.no_grad():
-        pred = model(inp)
-    return pred
-
-
-def eval(loader, model, data_norm=None, verbose=False):
-    model.eval()
-    if data_norm is None:
-        data_norm = {
-            'inp': {'sub': [0], 'div': [1]},
-            'gt': {'sub': [0], 'div': [1]}
-        }
-
-    t = data_norm['inp']
-    inp_sub = torch.FloatTensor(t['sub']).view(1, -1, 1, 1).cuda()
-    inp_div = torch.FloatTensor(t['div']).view(1, -1, 1, 1).cuda()
-    t = data_norm['gt']
-    gt_sub = torch.FloatTensor(t['sub']).view(1, 1, -1).cuda()
-    gt_div = torch.FloatTensor(t['div']).view(1, 1, -1).cuda()
-
-    max_psnr = 0
-    max_ssim = 0
-
-    val_psnr = utils.Averager()
-    val_ssim = utils.Averager()
-
-    pbar = tqdm(loader, leave=False, desc='val')
-    for batch in pbar:
-        for k, v in batch.items():
-            batch[k] = v.cuda()
-        inp = (batch['inp'] - inp_sub) / inp_div
-
-
-        pred = batched_predict(model,inp)
-        pred = (pred * gt_div + gt_sub).clamp_(0, 1)
-
-        psnr = utils.calc_psnr(pred, batch['gt'])
-        val_psnr.add(psnr.item(), inp.shape[0])
-
-        ssim = utils.ssim(pred, batch['gt'])
-        val_ssim.add(ssim.item(), inp.shape[0])
-
-        if psnr > max_psnr:
-            max_psnr = psnr
-            #save_image(pred, f"testimg/max_psnr.jpg", nrow=int(math.sqrt(pred.shape[0])))
-        if ssim > max_ssim:
-            max_ssim = ssim
-            #save_image(pred, f"testimg/max_ssim.jpg", nrow=int(math.sqrt(pred.shape[0])))
-
-        if verbose:
-            pbar.set_description(f'PSNR {val_psnr.item():.4f} SSIM {val_ssim.item():.4f}')
-
-    return val_psnr.item(), val_ssim.item()
-
+from train_L0 import batched_predict, eval
 
 
 if __name__ == '__main__':
