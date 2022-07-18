8a6f84e838ba48a0042b73ffeab06db34dee2112
arcobaleno <deityxsky@foxmail.com>
2022-07-18 11:02:12 +0800

    +.sh

---
+++ scripts/task.sh
@@ -0,0 +1,8 @@
+
+
+python train_xl1.py --config configs/train_WHURS19/WHURS19_drsensx2.yaml
+python train_xl1.py --config configs/train_WHURS19/WHURS19_drsensx4.yaml
+python train_xl1.py --config configs/train_RSSCN7/RSSCN_carnx8.yaml
+python train_x.py --config configs/train_RSSCN7/RSSCN_dasrx8.yaml
+
+
